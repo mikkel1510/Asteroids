9890d78077a5ffb16623921d208382c2bce659b9
Mikkel Nielsen <mikkel1510@live.dk>
2025-05-14 11:53:43 +0200

    - Basic Spring Boot microservice

---
+++ AsteroidsFX/Collision/src/test/java/dk/sdu/cbse/collisionsystem/CollisionDetectorTest.java
@@ -0,0 +1,13 @@
+package dk.sdu.cbse.collisionsystem;
+
+import junit.framework.TestCase;
+
+public class CollisionDetectorTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
